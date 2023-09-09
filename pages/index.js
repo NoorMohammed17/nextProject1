cb44887dc72e2e63f9d143071fc6036f02b893a0
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-09-09 21:40:25 +0530

    index file added

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+function HomePage() {
+    const DUMMY_MEETUPS = [
+        {
+            id : 1,
+            title : 'first one',
+            image : 'https://images.pexels.com/photos/2598630/pexels-photo-2598630.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=700&w=1200',
+            address : 'fiest oine meetup',
+            description : 'first'
+        },
+        {
+            id : 1,
+            title : 'second one',
+            image : 'https://images.pexels.com/photos/2598630/pexels-photo-2598630.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=700&w=1200',
+            address : 'second oine meetup',
+            description : 'second'
+        }
+    ]
+    return(
+        <div>
+            <MeetupList meetups={DUMMY_MEETUPS}/>
+
+        </div>
+    )
+}
+export default HomePage;
